3b0a289f94d8328db5c4721fff2e450c46a6b5c7
a-santiago <santiago.62596@gmail.com>
2021-10-05 10:16:45 -0400

    commit_002
    Fixed some logos and fixed some coloring

--- js/site.js
+++ js/site.js
@@ -1,9 +1,12 @@
-
 //Displays a message to the User
-function displayMessage() {
-    let msg = "";
-    msg =document.getElementById("message").value;
-    Swal.fire(msg);
-    
-}
+function displayHundo() {
+    let startValue = 0;
+    let endValue = 100;
+    // Get the numbers from the UI
+    startValue = document.getElementById("startValue").value;
+    endValue = document.getElementById("endValue").value;
+    // alert the User
+    alert("Start Value = " + startValue + " End Value" +
+        endValue);
 
+}
